f050006589d1d7f84b57f28bc5914945f31ca4da
benkline <ben.kline@gmail.com>
2016-12-11 16:49:57 -0700

    messages sending

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,20 +1,22 @@
 (function() {
-  function Message($firebaseArray) {
+    function Message($firebaseArray) {
 
-    var ref = firebase.database().ref().child('messages');
-    var allMessages = $firebaseArray(ref);
-    
-    return {
-        getByRoomId: function(room){
-          var messages = allMessages.orderByChild("roomId").equalTo(room)
-          return messages;
-        },
+        return {
+            getByRoomId: function(room){
+                var ref = firebase.database().ref().child('messages').orderByChild("roomId").equalTo(room)
+                var messages = $firebaseArray(ref);
+                return messages;
+            },
 
-        send: function(messageObj){
-          return messages.$add(messageObj);
+            send: function(messageObj){
+                var database = firebase.database().ref().child('messages')
+                var newMessageRef = database.push();
+                return newMessageRef.set(messageObj);
+                console.log(messageObj);
+
+            }
         }
     }
-}
 
 angular
   .module('cha-cha')
